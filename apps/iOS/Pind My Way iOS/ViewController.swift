d81bfa93d05daba47eb943f893f48b4c728c7bc0
bhuvan21 <bhuvan.belur@westminster.org.uk>
2018-09-30 22:43:30 +0100

    added marker placement and basic route drawing (sorry atto (but it's pretty bad you can rewrite it))

--- apps/iOS/Pind My Way iOS/ViewController.swift
+++ apps/iOS/Pind My Way iOS/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import SwiftyJSON
 
 let spaceOnTop: CGFloat = 100.0
 let spaceOnBottom: CGFloat = 100.0
@@ -17,7 +18,10 @@ class ViewController: UIViewController {
     var locationManager = CLLocationManager()
     var currentLocation: CLLocation? = nil
     var mapView: GMSMapView!
+    
     var zoomLevel: Float = 15.0
+    
+    var marker : GMSMarker = GMSMarker()
     
     @IBOutlet weak var addressField: UITextField!
 
@@ -27,12 +31,14 @@ class ViewController: UIViewController {
         
         addressField.delegate = self
         
+        
         locationManager = CLLocationManager()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.distanceFilter = 50
         locationManager.startUpdatingLocation()
         locationManager.delegate = self
+        
         
         let camera = GMSCameraPosition.camera(withLatitude: 0.0, longitude: 0.0, zoom: zoomLevel)
         self.mapView = GMSMapView.map(withFrame: CGRect(x: self.view.bounds.minX, y: self.view.bounds.minY+spaceOnTop, width: self.view.bounds.width, height: self.view.bounds.height-(spaceOnTop+spaceOnBottom)), camera: camera)
@@ -41,9 +47,51 @@ class ViewController: UIViewController {
         
         mapView.settings.myLocationButton = true
         mapView.isMyLocationEnabled = true
+        mapView.delegate = self
     }
+    
+
+    // Gets directions and draws them on the map
+    func getDirections(endCoordinates: CLLocationCoordinate2D) {
+        let startCoordinates: CLLocationCoordinate2D = currentLocation!.coordinate
+        //let endCoordinates: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 51.498142, longitude: -0.128676)
+        DispatchQueue.global(qos: .userInitiated).async {
+            DirectionManager.getDirections(from: startCoordinates, to: endCoordinates) { json in
+                let routes = json!["routes"].array!
+                self.drawRoute(route: routes[0])
+            }
+        }
+    }
+    
+    // Takes a route, gets the overview polyline, decodes it and draws it
+    func drawRoute(route: JSON) {
+        let points = String(describing: route["overview_polyline"]["points"])
+        let path = GMSPath(fromEncodedPath: points)
+        let polyline = GMSPolyline(path: path)
+        polyline.strokeWidth = 3.0
+        polyline.strokeColor = UIColor.red
+        polyline.map = mapView
+    }
+    
+    @IBAction func markerLocationConfirmed(_ sender: UIButton) {
+        getDirections(endCoordinates: marker.position)
+    }
+    
 
 }
+
+
+extension ViewController: GMSMapViewDelegate {
+    // Called when user touches to place a marker
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D){
+        print("Marker placed at \(coordinate.latitude), \(coordinate.longitude)")
+        mapView.clear()
+        marker = GMSMarker(position: coordinate)
+        marker.appearAnimation = GMSMarkerAnimation.pop
+        marker.map = mapView
+    }
+}
+
 
 extension ViewController: CLLocationManagerDelegate {
     // Handle incoming location events.
@@ -76,6 +124,7 @@ extension ViewController: CLLocationManagerDelegate {
         }
     }
     
+    
     // Handle location manager errors.
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         locationManager.stopUpdatingLocation()
@@ -85,16 +134,11 @@ extension ViewController: CLLocationManagerDelegate {
 
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("yeeting")
         textField.resignFirstResponder()
         
         if currentLocation != nil {
-            let startCoordinates: CLLocationCoordinate2D = currentLocation!.coordinate
-            let endCoordinates: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 51.498142, longitude: -0.128676)
-            DispatchQueue.global(qos: .userInitiated).async {
-                DirectionManager.getDirections(from: startCoordinates, to: endCoordinates) { json in
-                    print(json ?? "nil")
-                }
-            }
+            self.getDirections(endCoordinates: CLLocationCoordinate2D(latitude: 51.498142, longitude: -0.128676))
         }
         
         return true
