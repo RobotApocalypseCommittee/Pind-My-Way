10feb2f7fab38e11b62da0fa67fc50a00286b987
MrAttoAttoAtto <atto.allas@gmail.com>
2018-10-02 20:16:34 +0100

    Added search bar to search stuff, you can click a polyline to select that route (full completion soon), map fixed, search button removed, moved some functions around, renamed something to utils, added settings attributions, added one more dependency

--- apps/iOS/Pind My Way iOS/ViewController.swift
+++ apps/iOS/Pind My Way iOS/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 import SwiftyJSON
+import Polyline
 
 let spaceOnTop: CGFloat = 50.0
 // Really hacky for now to ignore the safe area
@@ -21,9 +22,17 @@ class ViewController: UIViewController {
     var currentLocation: CLLocation? = nil
     var mapView: GMSMapView!
     
+    var addressResultsViewController: GMSAutocompleteResultsViewController?
+    var addressSearchController: UISearchController?
+    
     var zoomLevel: Float = 15.0
     
     var marker : GMSMarker = GMSMarker()
+    
+    var routePolylines: Array<Array<GMSPolyline>> = []
+    var routeCoordinates: Array<Array<CLLocationCoordinate2D>> = []
+
+    @IBOutlet weak var addressBar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,23 +47,43 @@ class ViewController: UIViewController {
         
         let window = (UIApplication.shared.delegate as! AppDelegate).window!
         
+        // Magic 5.0 is magic
         let camera = GMSCameraPosition.camera(withLatitude: 0.0, longitude: 0.0, zoom: zoomLevel)
-        self.mapView = GMSMapView.map(withFrame: CGRect(x: self.view.bounds.minX, y: window.safeAreaInsets.top+spaceOnTop, width: self.view.bounds.width, height: self.view.bounds.height-(spaceOnTop+spaceOnBottom+(window.safeAreaInsets.bottom+window.safeAreaInsets.top))), camera: camera)
+        self.mapView = GMSMapView.map(withFrame: CGRect(x: self.view.bounds.minX, y: window.safeAreaInsets.top+spaceOnTop+5.0, width: self.view.bounds.width, height: self.view.bounds.height-(spaceOnTop+spaceOnBottom+(window.safeAreaInsets.bottom+window.safeAreaInsets.top))), camera: camera)
         
         self.view.addSubview(mapView)
         
         mapView.settings.myLocationButton = true
         mapView.isMyLocationEnabled = true
         mapView.delegate = self
+        
+        addressResultsViewController = GMSAutocompleteResultsViewController()
+        addressResultsViewController?.delegate = self
+        
+        addressSearchController = UISearchController(searchResultsController: addressResultsViewController)
+        addressSearchController?.searchResultsUpdater = addressResultsViewController
+        
+        let searchView = UIView(frame: CGRect(x: 0, y: window.safeAreaInsets.top, width: self.view.bounds.width, height: spaceOnTop))
+        
+        searchView.addSubview((addressSearchController?.searchBar)!)
+        self.view.addSubview(searchView)
+        addressSearchController?.searchBar.sizeToFit()
+        addressSearchController?.searchBar.isTranslucent = false
+        addressSearchController?.searchBar.placeholder = "Enter a location"
+        
+        definesPresentationContext = true
     }
     
 
     // Gets directions and draws them on the map
     func getDirections(endCoordinates: CLLocationCoordinate2D) {
         let startCoordinates: CLLocationCoordinate2D = currentLocation!.coordinate
+        
+        routePolylines = []
+        routeCoordinates = []
 
         DispatchQueue.global(qos: .userInitiated).async {
-            DirectionManager.getDirections(from: startCoordinates, to: endCoordinates) { json in
+            Utils.getDirections(from: startCoordinates, to: endCoordinates) { json in
                 let routes = json!["routes"].array!
                 if routes.count == 0 {
                     self.mapView.clear()
@@ -81,7 +110,7 @@ class ViewController: UIViewController {
                 }
                 
                 DispatchQueue.main.async {
-                    let update = GMSCameraUpdate.fit(GMSCoordinateBounds(coordinate: southWestCoordinates, coordinate: northEastCoordinates), withPadding: 75.0)
+                    let update = GMSCameraUpdate.fit(GMSCoordinateBounds(coordinate: southWestCoordinates, coordinate: northEastCoordinates), with: UIEdgeInsets(top: 75.0, left: 15.0, bottom: 15.0, right: 15.0))
                     self.mapView.animate(with: update)
                     
                     // I do these as two seperate things so the resize happens first of all
@@ -95,43 +124,74 @@ class ViewController: UIViewController {
     
     // Takes a route, gets the overview polyline, decodes it and draws it
     func drawRoute(route: JSON, asSelected selected: Bool) {
-        let points = String(describing: route["overview_polyline"]["points"])
-        let path = GMSPath(fromEncodedPath: points)
+        var points: Array<CLLocationCoordinate2D> = []
+
+        for leg in route["legs"].array! {
+            for step in leg["steps"].array! {
+                let stepPolyline = Polyline(encodedPolyline: step["polyline"]["points"].string!)
+                points.append(contentsOf: stepPolyline.coordinates!)
+            }
+        }
+
+        routeCoordinates.append(points)
+        
+        let overallPolyline: String = encodeCoordinates(points)
+        let path = GMSPath(fromEncodedPath: overallPolyline)
+        
         let polyline = GMSPolyline(path: path)
         polyline.strokeWidth = selected ? 3.0 : 2.5
         polyline.strokeColor = selected ? UIColor(red: 26.0/255, green: 142.0/255, blue: 255.0/255, alpha: 1.0) : UIColor.lightGray
         polyline.zIndex = selected ? 100 : 2
         polyline.map = mapView
-
+        
         let underPolyline = GMSPolyline(path: path)
         underPolyline.strokeWidth = selected ? 6.0 : 5.0
         underPolyline.strokeColor = selected ? UIColor(red: 26.0/255, green: 100.0/255, blue: 255.0/255, alpha: 1.0) : UIColor.gray
         underPolyline.zIndex = selected ? 99 : 1
         underPolyline.map = mapView
-    }
-    
-    @IBAction func searchButtonPressed(_ sender: Any) {
-        let autocompleteViewController = GMSAutocompleteViewController()
-        autocompleteViewController.delegate = self
         
-        if currentLocation != nil {
-            // Basically sets reasonable bounds to bias for in the place search
-            let autocompleteBiasBounds = GMSCoordinateBounds(coordinate: CLLocationCoordinate2D(latitude: currentLocation!.coordinate.latitude+0.1, longitude: currentLocation!.coordinate.longitude+0.1), coordinate: CLLocationCoordinate2D(latitude: currentLocation!.coordinate.latitude-0.1, longitude: currentLocation!.coordinate.longitude-0.1))
-            autocompleteViewController.autocompleteBounds = autocompleteBiasBounds
-        }
-        
-        self.present(autocompleteViewController, animated: true, completion: nil)
+        routePolylines.append([polyline, underPolyline])
     }
-    
 }
 
 
 extension ViewController: GMSMapViewDelegate {
     // Called when user touches to place a marker
-    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D){
-        print("Marker placed at \(coordinate.latitude), \(coordinate.longitude)")
-        placeMarker(mapView, at: coordinate)
-        getDirections(endCoordinates: coordinate)
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        if routePolylines.count == 0 {
+            print("Marker placed at \(coordinate.latitude), \(coordinate.longitude)")
+            placeMarker(mapView, at: coordinate)
+            getDirections(endCoordinates: coordinate)
+        } else {
+            var smallestDistances: Array<Double> = []
+            for coordinatesForRoute in routeCoordinates {
+                smallestDistances.append(coordinatesForRoute.map { Utils.getDistance(between: coordinate, and: $0) }.min()!)
+            }
+
+            // 15.60405 is the zoom level at which 50m is appropriate
+            // When you get into very zoomed in levels, google maps gets the point at which you clicked slightly wrong lat-long-wise, so if it's under 30m, we assume you tried to click it anyway
+            if smallestDistances.min() != nil && (smallestDistances.min()! < 30 || smallestDistances.min()! < Double(50 * pow(2, 15.60405-mapView.camera.zoom))) {
+                let indexOfSelected = smallestDistances.firstIndex(of: smallestDistances.min()!)!
+                
+                for i in 0...routePolylines.count-1 {
+                    let selected = i == indexOfSelected
+                    
+                    let polyline = routePolylines[i][0]
+                    polyline.strokeWidth = selected ? 3.0 : 2.5
+                    polyline.strokeColor = selected ? UIColor(red: 26.0/255, green: 142.0/255, blue: 255.0/255, alpha: 1.0) : UIColor.lightGray
+                    polyline.zIndex = selected ? 100 : 2
+                    
+                    let underPolyline = routePolylines[i][1]
+                    underPolyline.strokeWidth = selected ? 6.0 : 5.0
+                    underPolyline.strokeColor = selected ? UIColor(red: 26.0/255, green: 100.0/255, blue: 255.0/255, alpha: 1.0) : UIColor.gray
+                    underPolyline.zIndex = selected ? 99 : 1
+                }
+            } else {
+                print("Marker placed at \(coordinate.latitude), \(coordinate.longitude)")
+                placeMarker(mapView, at: coordinate)
+                getDirections(endCoordinates: coordinate)
+            }
+        }
     }
     
     func placeMarker(_ mapView: GMSMapView, at coordinate: CLLocationCoordinate2D) {
@@ -152,6 +212,12 @@ extension ViewController: CLLocationManagerDelegate {
         let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
                                               longitude: location.coordinate.longitude,
                                               zoom: zoomLevel)
+        
+        // Basically sets reasonable bounds to bias for in the place search
+        let autocompleteBiasBounds = GMSCoordinateBounds(coordinate: CLLocationCoordinate2D(latitude: location.coordinate.latitude+0.1, longitude: location.coordinate.longitude+0.1), coordinate: CLLocationCoordinate2D(latitude: location.coordinate.latitude-0.1, longitude: location.coordinate.longitude-0.1))
+        
+        addressResultsViewController?.autocompleteBounds = autocompleteBiasBounds
+        
         
         if currentLocation == nil {
             mapView.animate(to: camera)
@@ -182,31 +248,26 @@ extension ViewController: CLLocationManagerDelegate {
     }
 }
 
-extension ViewController: GMSAutocompleteViewControllerDelegate {
-    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+extension ViewController: GMSAutocompleteResultsViewControllerDelegate {
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didAutocompleteWith place: GMSPlace) {
         print(place)
-        dismiss(animated: true, completion: nil)
+        
+        addressSearchController?.isActive = false
         
         placeMarker(mapView, at: place.coordinate)
         
         getDirections(endCoordinates: place.coordinate)
     }
     
-    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
-        print("Error: \(error)")
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didFailAutocompleteWithError error: Error) {
+        print("Error: ", error.localizedDescription)
     }
     
-    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
-        print("Autocomplete cancelled")
-        dismiss(animated: true, completion: nil)
-    }
-    
-    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    func didRequestAutocompletePredictions(forResultsController resultsController: GMSAutocompleteResultsViewController) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
     }
     
-    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    func didUpdateAutocompletePredictions(forResultsController resultsController: GMSAutocompleteResultsViewController) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
-    
 }
