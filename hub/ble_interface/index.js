c591fb94cd3903084969b7eb56ab19abd6708511
Joe <joe.bell34@btinternet.com>
2019-02-20 12:02:54 +0000

    Start Debugging

--- hub/ble_interface/index.js
+++ hub/ble_interface/index.js
@@ -1,4 +1,5 @@
 const bleno = require("bleno")
+/*
 const bleConstants = require("./bleConstants.json")
 
 const versionCharacteristic = require("./version_characteristic")
@@ -43,7 +44,35 @@ bleno.on('disconnect', (cA)=>{
     console.log("Stopped")
     bleno.startAdvertising(bleConstants.deviceName, [bleConstants.primaryService]);
   })
-}
+});
+*/
+var BlenoPrimaryService = bleno.PrimaryService;
+
+var EchoCharacteristic = require('./characteristic');
+
+console.log('bleno - echo');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('echo', ['ec00']);
+  } else {
+    bleno.stopAdvertising();
+  }
 });
 
+bleno.on('advertisingStart', function(error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
 
+  if (!error) {
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new EchoCharacteristic()
+        ]
+      })
+    ]);
+  }
+});
