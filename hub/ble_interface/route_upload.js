c2a3017ae71a863c933e014492cf70065a5bec1c
Joe <joe.bell34@btinternet.com>
2019-02-22 17:11:13 +0000

    Add route upload

--- hub/ble_interface/route_upload.js
+++ hub/ble_interface/route_upload.js
@@ -1,17 +1,24 @@
-const bleno = require("bleno")
-const {Route} = require("../route")
+var util = require('util');
+
+var bleno = require("bleno")
 const coordinator = require("../coordinator").getInstance()
+const {Route} = require("../route")
+const bleConstants = require("./bleConstants")
 
-const {characteristics:{routeUpload}} = require("./bleConstants.json")
+var BlenoCharacteristic = bleno.Characteristic;
 
-let route_upload_characteristic = new bleno.Characteristic({
-  uuid: routeUpload.uuid, // or 'fff1' for 16-bit
-  properties: [ "write" ], // can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
-  onWriteRequest: function(data, offset, withoutResponse, callback) {
-    console.log("Write Request")
+class RouteUploadCharacteristic {
+  constructor() {
+    RouteUploadCharacteristic.super_.call(this, {
+      uuid: bleConstants.characteristics.routeUpload.uuid,
+      properties: ['write'],
+      value: null
+    });
+  }
+
+  onWriteRequest(data, offset, withoutResponse, callback) {
+    console.log("Received route:")
     console.log("Data: ", data)
-    console.log("Offset: ", offset)
-    console.log("WithoutResponse: ", withoutResponse)
     // Hoping it works
     let route = new Route()
     if (route.decode_data(data)) {
@@ -21,7 +28,7 @@ let route_upload_characteristic = new bleno.Characteristic({
       callback(bleno.Characteristic.RESULT_INVALID_ATTRIBUTE_LENGTH)
     }
   }
+}
 
-})
-
-module.exports = route_upload_characteristic
+util.inherits(RouteUploadCharacteristic, BlenoCharacteristic)
+module.exports = RouteUploadCharacteristic;
